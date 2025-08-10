00c15864a8340887bd7db3e2c04760de5c0f6586
M-ali676 <solangimuhammadali676@gmail.com>
2025-08-10 12:30:52 +0500

    Create app.js

---
+++ app.js
@@ -0,0 +1,52 @@
+// //____________06-08-25________________//
+// var test = "12345"
+// test = "testing"
+
+// var test2;
+// test2 = true;
+// console.log(test2)
+
+// var test2 = "SMIT"
+// console.log(test2);
+
+// var best2 = "Whatsapp"
+
+// console.log(best2);
+
+// var good = "facebook"
+// console.log(good);
+
+// var Young = "OLD"
+// console.log(Young);
+
+// var Math = "SCIENCE"
+// console.log(Math);
+
+// var my_name = "Muhammad Ali"
+// var num = 123456;
+// var boolean = true;
+// console.log(my_name, num, boolean, 123456);
+
+
+// var cast_name = "Solangi"
+// var num = 428567;
+// var midterm = false;
+// console.log(cast_name, num, midterm, 428567);
+
+// var compney_name = "Honda"
+// var num = 13579;
+// var longterm = this;
+// console.log(compney_name, num,  longterm, 13579);
+
+// var country_name = "Pakistan"
+// var num = 12345678;
+// var scanterm = true;
+// console.log(country_name, num, scanterm);
+
+// var school_name = "Qazi Sultan"
+// var num = 45678910
+// var attendence = true;
+// console.log(school_name, num, location, 45678910);
+
+
+
